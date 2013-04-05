ebc7f90bddf28d7f2501f478738acca0d3e36aec
Chenyang Lei <leichenyang@gmail.com>
2013-04-05 15:29:10 -0400

    add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meet1::Application.config.secret_token = 'f37ab9e2fd99504745536d523044463e4b125883f7e17a274bcf91038d6571471ccb83b51b0f10b3e5528b29ffb40ae3035f2844d8881da3ada4a4ce80efcc34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Meet1::Application.config.session_store :cookie_store, key: '_meet1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Meet1::Application.config.session_store :active_record_store
